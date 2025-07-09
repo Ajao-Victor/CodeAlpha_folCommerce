98eac426e55b807fcf2583a84be09e72f0e9233c
AJ Stack <AJ_Stack@AJ-Stacks-MacBook-Pro.local>
2025-07-09 08:48:04 +0100

    backend url update

--- server/server.js
+++ server/server.js
@@ -223,4 +223,6 @@ app.post('/checkout', authenticateToken, async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server running on http://127.0.0.1:3000'));
+// app.listen(3000, () => console.log('Server running on http://127.0.0.1:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
